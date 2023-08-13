0584d7fd36f6e090c8b269b36f886f3a4755575d
kmfiguerrez <kmfiguerrez@gmail.com>
2023-08-13 09:52:10 +0800

    Install @clerk/themes package.
    - Configure the clerk switcher component to adapt
      to dark themes.

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,7 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
-
+import { dark } from '@clerk/themes'
 
 const Topbar = () => {
   return (
@@ -37,6 +37,7 @@ const Topbar = () => {
 
         <OrganizationSwitcher 
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: 'py-2 px-4'
             }
